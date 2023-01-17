0a2f807c9f0402419ad894947b59cdec0e2f051e
Georgi Nakov <nakov.gl@gmail.com>
2023-01-17 18:30:28 +0000

    more lexing

--- src/Lex.hs
+++ src/Lex.hs
@@ -6,6 +6,7 @@ import Data.Char
 import Data.List
 import Control.Monad
 import Control.Applicative
+import Control.Arrow ((***))
 import qualified Data.Text as T
 import qualified Data.Map as M
 
@@ -14,6 +15,7 @@ import Bwd
 data Kin
   = Nom
   | Key
+  | Blk
   | Sym
   | Grp Grouping [Tok]
   | Spc
@@ -28,7 +30,10 @@ data Tok = Tok
   , raw :: String
   } deriving Show
 
-data Grouping = Literal | Comment | Error
+data Grouping = Literal | Comment | Block | Bracket Bracket | Error
+  deriving (Show, Eq)
+
+data Bracket = Round | Square | Curly
   deriving (Show, Eq)
 
 type Pos = (Int, Int) -- line, column with 0 origin
@@ -39,8 +44,34 @@ instance Eq Tok where
 type Doc = (T.Text, Pos)
 
 lexer :: T.Text -> [Tok]
-lexer = lex1 . lex0
+lexer = lex3 . lex2 . lex1 . lex0
 
+-- basic lexing 
+lex0 :: T.Text -> [Tok]
+lex0 t = unfoldr tok (t, (0, 0)) where
+  tok :: Doc -> Maybe (Tok, Doc)
+  tok d@(_, p) = case munch d symbols of
+    Just (s, d) -> return (Tok Sym p s, d)
+    Nothing -> peek d >>= \case
+      (c, d)
+        | isAlpha c ->
+          let (s, d') = spand (\ c -> isAlphaNum c || c == '_') d
+              x = c : s
+              k = case M.lookup x keywords of
+                Just False -> Key
+                Just True  -> Blk
+                _          -> Nom
+          in  return (Tok k p x, d')
+        | isNL c -> return (Tok Ret p [c], d)
+        | isSpace c -> let (s, d') = spand (\ c -> isSpace c && not (isNL c)) d in
+            return (Tok Spc p (c : s), d')
+        | isDigit c -> let (s, d') = spand isDigit d in
+            return (Tok Dig p (c : s), d')
+        | otherwise -> return (Tok Urk p [c], d)
+
+
+-- grouping character literals (surrounded by `'`), end of line comments (`%`)
+-- and multiline comments (`%{` and `%}`)
 lex1 :: [Tok] -> [Tok]
 lex1 = normal where
   normal [] = []
@@ -50,45 +81,67 @@ lex1 = normal where
     | t == percentopenbrace = mlcomment (B0 :< (B0 :< t)) ts
     | otherwise = t : normal ts
 
-  charlit acc [] = [group Error acc]
+  charlit acc [] = [grp Error acc]
   charlit acc (t:ts)
-    | t == squote = group Literal (acc :< t) : normal ts
-    | kin t == Ret = group Error acc : normal (t:ts)
+    | t == squote = grp Literal (acc :< t) : normal ts
+    | kin t == Ret = grp Error acc : normal (t:ts)
     | otherwise = charlit (acc :< t) ts
 
-  lcomment acc [] = [group Comment acc]
+  lcomment acc [] = [grp Comment acc]
   lcomment acc (t:ts)
-    | kin t == Ret = group Comment acc : normal (t:ts)
+    | kin t == Ret = grp Comment acc : normal (t:ts)
     | otherwise = lcomment (acc :< t) ts
 
   mlcomment B0 _ = error "stack should never be empty"
-  mlcomment (az :< a) [] = let c0 = group Error a in
+  mlcomment (az :< a) [] = let c0 = grp Error a in
     case az of
       B0 -> [c0]
       az :< a -> mlcomment (az :< (a :< c0)) []
   mlcomment acc@(az :< a) (t:ts)
     | t == percentopenbrace = mlcomment (acc :< (B0 :< t)) ts
-    | t == percentclosbrace = let c0 = group Comment (a :< t) in
+    | t == percentclosbrace = let c0 = grp Comment (a :< t) in
         case az of
           B0 -> c0 : normal ts
           az :< a -> mlcomment (az :< (a :< c0)) ts
     | otherwise = mlcomment (az :< (a :< t)) ts
-
-
-
-  group :: Grouping -> Bwd Tok -> Tok
-  group grp tz = case tz <>> [] of
-    ts@(t:_) -> Tok (Grp grp ts) (pos t) (ts >>= raw)
-    [] -> error "should not make empty group"
 
   squote = Tok Sym (0,0) "'"
   percent = Tok Sym (0,0) "%"
   percentopenbrace = Tok Sym (0,0) "%{"
   percentclosbrace = Tok Sym (0,0) "%}"
 
+-- finds blocks delimited by a Blk keyword and `end`
+lex2 :: [Tok] -> [Tok]
+lex2 = helper B0 where
+  helper B0 [] = []
+  helper (az :< a) [] = helper az [grp Block a]
+  helper az (t : ts)
+    | kin t == Blk  = helper (az :< (B0 :< t)) ts
+    | t == end = case az of
+        az :< a -> helper az $ grp Block (a :< t) : ts
+        B0 -> helper B0 $ grp Error (B0 :< t) : ts
+  helper B0 (t : ts) = t : helper B0 ts
+  helper (az :< a) (t : ts) = helper (az :< (a :< t)) ts
 
-lex0 :: T.Text -> [Tok]
-lex0 t = unfoldr tok (t, (0, 0))
+  end = Tok Key (0,0) "end"
+
+-- finds brackets inside blocks but not vice versa
+lex3 :: [Tok] -> [Tok]
+lex3 = helper B0 where
+  helper B0 [] = []
+  helper (az :< (_, a)) [] = helper az [grp Error a]
+  helper az (t : ts)
+    | Just b <- opener t = helper (az :< (b, B0 :< t)) ts
+  helper (az :< (b, a)) (t : ts)
+    | t == closer b = helper az $ grp (Bracket b) (a :< t) : ts
+    | otherwise = helper (az :< (b, a :< t)) ts
+  helper B0 (Tok (Grp Block ss) p s : ts) = Tok (Grp Block $ lex3 ss) p s : helper B0 ts
+  helper B0 (t : ts)  = t : helper B0 ts
+
+grp :: Grouping -> Bwd Tok -> Tok
+grp g tz = case tz <>> [] of
+  ts@(t:_) -> Tok (Grp g ts) (pos t) (ts >>= raw)
+  [] -> error "should not make empty group"
 
 unix :: T.Text -> T.Text
 unix t = case T.uncons t of
@@ -98,25 +151,6 @@ unix t = case T.uncons t of
       Just ('\n', _) -> unix t
       _ -> T.cons '\n' (unix t)
     _ -> T.cons c (unix t)
-
-tok :: Doc -> Maybe (Tok, Doc)
-tok d@(_, p) = case munch d symbols of
-  Just (s, d) -> return (Tok Sym p s, d)
-  Nothing -> peek d >>= \case
-    (c, d)
-      | isAlpha c ->
-        let (s, d') = spand (\ c -> isAlphaNum c || c == '_') d
-            x = c : s
-            k = case M.lookup x keywords of
-              Just _ -> Key
-              _      -> Nom
-        in  return (Tok k p x, d')
-      | isNL c -> return (Tok Ret p [c], d)
-      | isSpace c -> let (s, d') = spand (\ c -> isSpace c && not (isNL c)) d in
-          return (Tok Spc p (c : s), d')
-      | isDigit c -> let (s, d') = spand isDigit d in
-          return (Tok Dig p (c : s), d')
-      | otherwise -> return (Tok Urk p [c], d)
 
 peek :: Doc -> Maybe (Char, Doc)
 peek (t, p) = T.uncons t >>= \ (x, t) -> return (x, (t, p `after` x))
@@ -152,8 +186,9 @@ munch d (Table b m) = (do
   return (c : s, d)
   ) <|> ("", d) <$ guard b
 
-keywords :: M.Map String ()
-keywords = M.fromList $ map (,()) ["if", "else", "elseif", "end", "function", "for"]
+keywords :: M.Map String Bool -- whether the keyword begins a block ending with `end`
+keywords = M.fromList (map (, True) ["if", "function", "for", "while", "switch"])
+         <> M.fromList (map (, False) ["else", "elseif", "case", "otherwise", "end"])
 
 symbols :: Table
 symbols = foldr insT empT
@@ -171,3 +206,13 @@ symbols = foldr insT empT
   , ".?"
   , "(", ")", "[", "]", "{", "}", ",", ";"
   ]
+
+
+opener :: Tok -> Maybe Bracket
+opener = flip lookup $ map (Tok Sym (0,0) *** id) [("(", Round), ("[", Square), ("{", Curly)]
+
+closer :: Bracket -> Tok
+closer b = Tok Sym (0,0) $ case b of
+   Round  -> ")"
+   Square -> "]"
+   Curly  -> "}"
