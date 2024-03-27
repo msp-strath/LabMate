a711daf37d412bdeb15de9097bf398ba6e4dae53
Fredrik Nordvall Forsberg <fredrik.nordvall-forsberg@strath.ac.uk>
2024-03-27 10:19:10 +0000

    [ broken ] add simple versions of res cal examples

---
+++ examples/rescal.m
@@ -0,0 +1,17 @@
+%> A :: [ i <- 9,  j <- 5 ] (if j == 0 then Q T else Q 1)
+A = [ t1 1 -1 0 0
+      t2 1 -1 0 0
+      t3 1 -1 0 0
+      t4 1 -1 0 0
+      t5 1 -1 0 0
+      t6 0 0 1 -1
+      t7 0 0 1 -1
+      t8 0 0 1 -1
+      t9 0 0 1 -1 ]
+
+%> B :: [ 5 x 1 ] int
+B = [ m; c; a; cOffset; aOffset ]
+
+C = A * B
+
+%> typeof C
