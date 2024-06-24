83fdb19387de50324e753542d44a4f9129b48d5f
Georgi Nakov <nakov.gl@gmail.com>
2024-06-24 15:44:33 +0100

    [ example ] matrixq.m

---
+++ examples/matrixq.m
@@ -0,0 +1,12 @@
+%> dimensions V for Q over `Length, `Mass, `Time
+
+%> unit metre :: Q({ `Length })
+%> unit kg :: Q({ `Mass })
+%> unit sec :: Q({ `Time })
+
+%> x :: [i <- [{} {`Length}] x j <- [{} {`Length}]] Q({`Mass * j / i})
+x = [ 2*kg       5*kg*metre
+      3*kg/sec   4*kg*metre/sec ]
+%%> y :: [j <- [{} {`Length} ] x k <- [{}, {1/`Mass}]] Q({j/k})
+%y = [ 7        3/kg
+%      5/metre  2/kg/metre ]
