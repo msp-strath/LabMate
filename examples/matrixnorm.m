79a822eb8de3ecd0a469834e5a248e8bd7c751f8
Fredrik Nordvall Forsberg <fredrik.nordvall-forsberg@strath.ac.uk>
2024-08-07 12:44:53 +0100

    [ fix ] some of the bugs

---
+++ examples/matrixnorm.m
@@ -0,0 +1,14 @@
+%> dimensions V for Q over `Length, `Mass, `Time
+
+%> unit metre :: Q({ `Length })
+%> unit kg :: Q({ `Mass })
+%> unit sec :: Q({ `Time })
+
+%> y :: Q({ `Mass})
+y = 2*kg
+
+%> typeof y
+
+x = [ 2*kg 2*kg ]
+
+%> typeof x
