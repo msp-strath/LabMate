e68eb159c0fab3b95a7e0296e04b3134ff4ed456
Fredrik Nordvall Forsberg <fredrik.nordvall-forsberg@strath.ac.uk>
2024-08-09 11:43:56 +0100

    Revert "[ broken ] starting to do interesting normalisation of matrix types"
    This reverts commit 5b4a40baf3a1fa2ca788fb1ec01ca215c7c61148.

--- src/Term/Thinning.hs
+++ src/Term/Thinning.hs
@@ -4,8 +4,6 @@ module Term.Thinning where
 import Term.Indexed
 import Term.Natty
 
-import MissingLibrary
-
 -- thinnings
 data (<=) :: Nat -> Nat -> * where
   No :: n <= m ->   n <= S m
@@ -112,8 +110,6 @@ cmpCov u u' = case cmpThin (covl u) (covl u') of
 data (^) :: (Nat -> *) -> Nat -> * where
   (:^) :: p n -> n <= m -> p ^ m
 
-instance IxFunctor (^) where
-  ixmap f (t :^ th) = f t :^ th
 
 scopeOf :: p ^ n -> Natty n
 scopeOf (_ :^ th) = bigEnd th
