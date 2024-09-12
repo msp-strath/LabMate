814f0199eba9f497a1297fadc4493391a0ca7c69
Fredrik Nordvall Forsberg <fredrik.nordvall-forsberg@strath.ac.uk>
2024-09-12 09:44:57 +0100

    [ demo ] add live copy, fix minor things

---
+++ writing/NPLSep2024/examples-live/demo_5_1_cubit.m
@@ -0,0 +1,12 @@
+%> dimensions D for Q over metre for `Length, kg for `Mass, sec for `Time
+
+inch = 0.0254*metre;
+foot = 12*inch;
+cubit = 0.462788*metre;
+
+x = 5*metre + 12*foot;
+%> typeof x
+
+y = x/cubit
+%> typeof y
+
